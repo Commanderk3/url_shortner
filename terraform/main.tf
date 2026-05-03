e2cd8e6d23a14577a39717bbee36b27ff05dcc0a
Commanderk3 <diwangshukakotygmail.com>
2026-05-04 00:34:22 +0530

    Remove output variables for instance details and mark SSH command as sensitive

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,19 +112,3 @@ resource "aws_instance" "k3s_server" {
   }
 }
 
-# Outputs
-output "instance_public_ip" {
-  value = aws_instance.k3s_server.public_ip
-  description = "Public IP of K3s server"
-}
-
-output "app_url" {
-  value = "http://${aws_instance.k3s_server.public_ip}:30080"
-  description = "URL to access the application"
-}
-
-output "ssh_command" {
-  value = "ssh -i ${var.key_name}.pem ubuntu@${aws_instance.k3s_server.public_ip}"
-  description = "SSH command to connect to the instance"
-  sensitive = true  # Mark as sensitive because it contains key name
-}

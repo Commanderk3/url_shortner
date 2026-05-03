6817cdf829f3a7c49b268fa446a1d2288a40d29a
Commanderk3 <diwangshukakotygmail.com>
2026-05-04 00:21:15 +0530

    Update AWS provider version to 4.0 to avoid GPG key issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,13 @@
 terraform {
   required_version = ">= 1.0"
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"  # Changed from 5.0 to 4.0
+      version = "~> 4.0"  # Using 4.0 to avoid GPG key issue
     }
   }
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+  
   backend "local" {
     path = "terraform.tfstate"
   }
